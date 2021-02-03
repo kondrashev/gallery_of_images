9440d0b06cb39c76b855872952d99d57fb36c3a7
Pavel Kondrashev <kondrashev@ukr.net>
2021-02-03 12:10:40 +0200

    1

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello Pavel!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  text: {
+    color: 'blue',
+    fontSize: 30
+  }
 });
